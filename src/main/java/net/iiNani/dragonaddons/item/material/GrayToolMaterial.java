a209a017ccb9bb1bda1ab931b4f776607afd3db0
iiNani <akmeduri101@gmail.com>
2022-11-17 15:16:39 -0800

    Balanced all Hoe items and added Gray Dragon tools

---
+++ src/main/java/net/iiNani/dragonaddons/item/material/GrayToolMaterial.java
@@ -0,0 +1,39 @@
+package net.iiNani.dragonaddons.item.material;
+
+import net.iiNani.dragonaddons.item.ModItems;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+
+public class GrayToolMaterial implements ToolMaterial {
+    public static final GrayToolMaterial INSTANCE = new GrayToolMaterial();
+
+    @Override
+    public int getDurability() {
+        return 8161;
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return 16;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return 19;
+    }
+
+    @Override
+    public int getMiningLevel() {
+        return 4;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 18;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(ModItems.GRAY_DRAGON_FRAGMENT);
+    }
+}
